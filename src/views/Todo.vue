4670da805b69d1e82b3ac2131a1112daea47e52e
ryton <ryton.jiang@gmail.com>
2019-11-01 15:47:08 +0800

    绑定输入框与todoList数组

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -3,14 +3,17 @@
     <h1>todo list</h1>
     <div class="todo_box">
       <div class="todo_input">
-        <input type="text" name id /><button>+</button>
+        <input type="text" name id v-model="addText"/>
+        <button @click="addTodo">+</button>
       </div>
       <div class="todo_list">
         <ul>
-          <li>list</li>
-          <li>list</li>
-          <li>list</li>
-          <li>list</li>
+          <li v-for="todo in todoList" :key="todo.content">
+            <input type="checkbox" name id />
+            <label for></label> {{todo.content}}
+            <button>X</button>
+          </li>
+          
         </ul>
       </div>
       <div class="todo_count">
@@ -24,10 +27,25 @@
 <script>
 export default {
   name: "Todo",
-  data(){
-    return{
-      addText:'',
-      todoList:[]
+  data() {
+    return {
+      addText: "",
+      todoList: []
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todoList.push({
+        content:this.addText,
+        compoleted:false
+      });
+      this.addText=''
+      console.log(this.todoList)
+    }
+  },
+  watch:{
+    addText(){
+      console.log(this.addText)
     }
   }
 };
